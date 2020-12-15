34cb6523cec675718ae07e828eda3a7bb1e6c514
Schenikova Snezhaa <snezha_00_97@mail.ru>
2020-12-15 16:08:51 +0300

    audio file generation function was modified

--- app.py
+++ app.py
@@ -5,6 +5,26 @@ from synthesize import VoiceGen
 
 app = Flask(__name__)
 
+generator = VoiceGen()
+
+
+@app.route('/record', methods=['POST'])
+def voice_result():
+    if request.method == 'POST':
+        file = request.files["voice"]
+        with open("static/recordings/recording.wav", "wb") as aud:
+            aud_stream = file.read()
+            aud.write(aud_stream)
+            aud.close()
+        if file:
+            global embedding
+            embedding = generator.encode()
+            data = "Successfully recorded!"
+            return json.dumps(data)
+        return "Bad request..."
+
+
+
 
 @app.route('/result', methods=['GET'])
 def result():
@@ -17,29 +37,13 @@ def result():
         return "No place information is given"
 
 
-@app.route('/record', methods=['POST'])
-def voice_result():
-    if request.method == 'POST':
-        file = request.files["voice"]
-        with open("static/recordings/recording.wav", "wb") as aud:
-            aud_stream = file.read()
-            aud.write(aud_stream)
-            aud.close()
-        if file:
-            data = "Successfully recorded!"
-            return json.dumps(data)
-        return "Bad request..."
-
-
 @app.route('/', methods=['GET'])
 def render():
     return render_template('index.html')
 
 def finish_work(str):
-    generator = VoiceGen()
-    embeding = generator.encode()
-    result, samplerate = generator.generate(str, embeding)
-    sf.write("/static/recordings/result.wav", result, samplerate)
+    result, samplerate = generator.generate(str, embedding)
+    sf.write("static/recordings/result.wav", result, samplerate)
 
 
 if __name__ == '__main__':
